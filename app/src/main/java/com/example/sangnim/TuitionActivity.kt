bf1a3dd2a624e3d310fb9a6b2ecb01b34fb0fd42
kimeunju99 <8520hana@naver.com>
2021-05-04 17:09:31 +0900

    0504_김은주_오류수정

--- app/src/main/java/com/example/sangnim/TuitionActivity.kt
+++ app/src/main/java/com/example/sangnim/TuitionActivity.kt
@@ -7,6 +7,9 @@ import androidx.appcompat.app.AppCompatActivity
 public class TuitionActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //TODO <진행 중인> 과외 리스트
+        setContentView(R.layout.tuition)
+        // 리스트() = 현재 진행 중인 과외 목록
+        // 리스트 아이템 클릭 -> tuition_select 페이지로 이동 , selecttuitionActivity
+        //플로팅 버튼 = 새과외 추가 -> tuition_new 레이아웃, 새 액티비티 필요
     }
 }
